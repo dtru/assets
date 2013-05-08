150ff7684d673825d43e761a731dded944052004
Ruslan Sukhar <rs@mttd.ru>
2013-05-08 16:14:23 +0400

    Fixed: uppercase assets serving

--- init.php
+++ init.php
@@ -1,9 +1,9 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 // Static file serving (CSS, JS, images)
-Route::set('assets/serve', 'assets/<file>', array('file' => '[a-z\d\_\-\/\.]+\.[a-z\d]{2,4}'))
+Route::set('assets/serve', 'assets/<file>', array('file' => '[a-zA-Z\d\_\-\/\.]+\.[a-z\d]{2,4}'))
 	->defaults(array(
-	'controller' => 'assets',
+	'controller' => 'Assets',
 	'action'     => 'serve',
 	'file'       => NULL,
 ));
