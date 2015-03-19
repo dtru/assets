09d886f7079f15963da3a07b0c037fb4d062b74b
soulwest <sovest21@yandex.ru>
2015-03-19 20:42:36 +0700

    fix route

--- init.php
+++ init.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 // Static file serving (CSS, JS, images)
-Route::set('assets/serve', 'assets/<file>', array('file' => '[a-zA-Z\d\_\-\/\.]+\.[a-z\d]{2,4}'))
+Route::set('assets/serve', 'assets/<file>', array('file' => '[a-zA-Z\d\_\-\/\.]+\.[a-z\d]{2,5}'))
 	->defaults(array(
 	'controller' => 'Assets',
 	'action'     => 'serve',
